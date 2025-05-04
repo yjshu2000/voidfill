25d4d3c45f86112c11c3501b772f9925df03c65a
Jacen <yjshu2017@gmail.com>
2025-05-04 18:38:27 -0400

    sshh updated version code thing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.upsiway.voidfill"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
